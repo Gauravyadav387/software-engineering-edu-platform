1d4760c031610ca9cf121ed228f48545242087a2
Lucky Kumar <lkrgupta098@ce.du.ac.in>
2026-04-14 14:33:29 +0530

    trigger vercl deploy

--- src/Pages/Teacher/Analytics.js
+++ src/Pages/Teacher/Analytics.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";// Analytics
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
